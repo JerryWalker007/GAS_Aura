e6f6cfaf3e30d5e6d94639aa7c1d231e894d3b75
JerryWalker007 <76182676+JerryWalker007@users.noreply.github.com>
2024-02-11 14:48:30 -0600

    Aura HUD

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,12 @@
+// Copyright Walker G&G
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
+
+
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Walker G&G
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
